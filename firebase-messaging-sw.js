ea7fdfe290aa08f938be325f80285e5f8b68cba4
BTD1GHOST <124221535+BTD1GHOST@users.noreply.github.com>
2026-02-04 13:28:56 -0500

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA9Mq0eCDuicDEejmtqCwlWnZ4otvz9FdY",
+  authDomain: "the-hub-f09c4.firebaseapp.com",
+  projectId: "the-hub-f09c4",
+  messagingSenderId: "992597104461",
+  appId: "1:992597104461:web:17ea5b0e3ab5d518804904"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.notification?.title || "New message";
+  const options = {
+    body: payload.notification?.body || "Someone sent a message",
+    icon: "/icon-192.png"
+  };
+
+  self.registration.showNotification(title, options);
+});
