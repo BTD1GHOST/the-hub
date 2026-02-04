10a897a8b983bbc2f6d555ae64d404f070741109
BTD1GHOST <124221535+BTD1GHOST@users.noreply.github.com>
2026-02-04 11:21:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// ===== Firebase imports =====
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9Mq0eCDuicDEejmtqCwlWnZ4otvz9FdY",
+  authDomain: "the-hub-f09c4.firebaseapp.com",
+  projectId: "the-hub-f09c4",
+  storageBucket: "the-hub-f09c4.firebasestorage.app",
+  messagingSenderId: "992597104461",
+  appId: "992597104461"
+};
+// ===== Initialize Firebase =====
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+console.log("Firebase connected");
